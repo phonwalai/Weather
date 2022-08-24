aa64734ffea3902c3ac7f86a6fc35034f5c66f82
phonwalai <87663393+phonwalai@users.noreply.github.com>
2022-08-24 10:39:02 +0700

    fixed test

---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by phonwalai on 24/8/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+        view.backgroundColor = .red
+    }
+}
+
